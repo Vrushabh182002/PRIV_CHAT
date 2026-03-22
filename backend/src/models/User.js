ab4fccc018610317d8844a4d7760117740f74291
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-03-22 12:10:22 +0530

    trimed and lowercased the username and email and renamed the panicpassowrd to panicPasswordHash as a string

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, unique: true },
+    username: { type: String, required: true, trim: true },
+    email: { type: String, unique: true, lowercase: true, trim: true },
     passwordHash: String,
-    panicPassword: String,
+
+    panicPasswordHash: String,
+
     role: {
       type: String,
       enum: ["USER", "ADMIN"],

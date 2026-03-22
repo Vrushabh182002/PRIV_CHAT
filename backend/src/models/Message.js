214785aa8a558d64ee2e9f8bd02a27c028933f7e
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-03-22 12:09:13 +0530

    added objectid from schema to the senderid

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,17 +1,34 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-  senderId: { type: ObjectId, ref: "User" },
-  messageType: { type: String, enum: ["text", "image", "file"] },
-  content: String,
-  fileMeta: {
-    fileName: String,
-    fileSize: Number,
-    fileType: String,
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    messageType: {
+      type: String,
+      enum: ["text", "image", "file"],
+      default: "text",
+    },
+    content: String,
+    fileMeta: {
+      fileName: String,
+      fileSize: Number,
+      fileType: String,
+    },
   },
-  createdAt: Date,
-});
+  {
+    timestamps: true,
+  },
+);
+
 messageSchema.index({ chatId: 1, createdAt: -1 });
 
 export default mongoose.model("Messages", messageSchema);

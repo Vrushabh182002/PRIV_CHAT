37142cc1f235cd9089d64836e1fbd0006b5366d4
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-03-22 12:14:55 +0530

    To check the host of the database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`DB Connected: ${mongoose.connection.host}`);
         console.log("Data Base Connected Successfully");
     } catch (error) {
         console.log("Data Base Connection Failed", error.message);

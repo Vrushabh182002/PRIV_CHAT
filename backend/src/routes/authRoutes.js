df4747f35d2e028f789f8462e166142fcc52e737
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-05-10 11:47:26 +0530

    Added logout and auth check route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  checkAuth,
+  logout,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/check-auth", checkAuth);
+router.post("/logout", logout);
 
 export default router;

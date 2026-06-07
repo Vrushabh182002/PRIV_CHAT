a7138a555adbb454c0f6dbae6e83b1e9a88d2d9b
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-06-07 21:17:17 +0530

    Added admin dashboard path and lazy loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,62 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import AdminRoute from "./security/AdminRoute";
 import PublicRoute from "./security/PublicRoute";
 import ProtectedRoute from "./security/ProtectedRoute";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Chats from "./pages/Chats";
+import Loader from "./components/Loader";
+
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Chats = lazy(() => import("./pages/Chats"));
+const AccountLock = lazy(() => import("./pages/AccountLock"));
+const AdminDashboard = lazy(() => import("./admin/AdminDashboard"));
 
 const App = () => {
   return (
     <>
       <main>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Chats />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            }
-          ></Route>
-          <Route
-            path="/register"
-            element={
-              <PublicRoute>
-                <Register />
-              </PublicRoute>
-            }
-          ></Route>
-        </Routes>
+        <Suspense
+          fallback={
+            <Loader className="flex items-center justify-center h-screen text-white bg-black" />
+          }
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Chats />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+            <Route path="/system-locked" element={<AccountLock />} />
+            <Route
+              path="/admin"
+              element={
+                // <AdminRoute>
+                  <AdminDashboard />
+                // {/* </AdminRoute> */}
+              }
+            />
+          </Routes>
+        </Suspense>
       </main>
     </>
   );

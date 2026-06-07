f117f1366b07d5fd9a2871d81d319e15b3101298
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-06-07 21:15:13 +0530

    Phase 1 started

---
+++ frontend/src/pages/AccountLock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AccountLock = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#0c0d0f] text-[#e8e9ed]  relative overflow-hidden px-4 sm:px-6">
+      hello
+    </div>
+  );
+};
+
+export default AccountLock;

3423bead65c0079197f69f46b8935c15c98acb1b
VRUSHABH SOMANI <vssomani2002@gmail.com>
2026-06-07 15:00:34 +0530

    Removed global unlock route, since it is not required anymore

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { unlockSystem, unlockUser } from "../controllers/adminController.js";
+import { unlockUser } from "../controllers/adminController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { adminOnly } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
-
-router.post("/unlock", protect, adminOnly, unlockSystem);
 
 router.put("/unlock-user/:id", protect, adminOnly, unlockUser);
 
